ebc4d3772044f39621ab1039746eec0b03698b37
MaxJ02 <114068285+MaxJ02@users.noreply.github.com>
2022-12-29 14:37:30 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "car.h"
 
 /********************************************************************************
-* Detta är versionen av main som använder Vtables för VG uppgiften. 
+* Detta är versionen av main som använder Vtables för VG uppgiften.
 ********************************************************************************/
 
 /**************************************************************************************************************************
@@ -22,28 +22,28 @@ int main(void)
 {
 
 	struct car car1;
-  struct car car2;
-  struct car car3;
-  car_init(&car1, "Volvo", "V70", "Black", 1995, MANUAL);
-  car_init(&car2, "BMW", "Z3", "Blue", 2001, AUTOMATIC);
-  car_init(&car3, "Skoda", "Octavia RS", "White", 2003, MANUAL);
- 
+	struct car car2;
+	struct car car3;
+	car_init(&car1, "Volvo", "V70", "Black", 1995, MANUAL);
+	car_init(&car2, "BMW", "Z3", "Blue", 2001, AUTOMATIC);
+	car_init(&car3, "Skoda", "Octavia RS", "White", 2003, MANUAL);
+
 	struct car* cars[] = { &car1, &car2, &car3 };
 
-  c.vptr->car_print(&car1, stdout);
-  c.vptr->car_print(stdout, &car2);
-  c.vptr->car_print(&car3, stdout);
+	car1.vptr->car_print(stdout, &car1);
+	car2.vptr->car_print(stdout, &car2);
+	car3.vptr->car_print(stdout,&car3);
 
-  c.vptr->change_color(&car3, "Green")
+	car3.vptr->change_color(&car3, "Green");
 
-  c.vptr->change_transmission(&c, MANUAL);  
-    
+	car3.vptr->change_transmission(&car3, MANUAL);
+
 	//car_change_transmission(&car3);
-        
+
 	//Skriver ut car3 igen för att kontrollera att ändringarna lyckades.
 	//car_print(&car3, stdout);
-  c.vptr->car_print(&car3, stdout);
-	
+	car3.vptr->car_print(stdout, &car3);
+
 	//Öppnar och skriver till filen "cars.txt", om filpekaren returnerar null skrivs ett felutmeddelande ut och 1 returneras för att indikera att ett fel har skett.
 	FILE* fp = fopen("cars.txt", "w");
 	if (fp == NULL) {
@@ -54,7 +54,7 @@ int main(void)
 	//Här skrivs innehållet ur cars arrayen ut till filen cars.txt via funktionen car_print
 	size_t i;
 	for (i = 0; i < 3; i++) {
-		car_print(cars[i], fp);
+		car_print(fp, cars[i]);
 	}
 
 	// Stänger filen cars.txt
